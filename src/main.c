9ee80aeb51869c1c240c99dc1529d5a8d56d5fea
Martha Cuenca Castillo <mcuenca-@cbr2s6.42barcelona.com>
2025-09-22 11:23:37 +0200

    [feat]: history
    env_v3.c: added shell level increment.
    
    main: split and added history.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mcuenca- <mcuenca-@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 14:04:03 by mcuenca-          #+#    #+#             */
-/*   Updated: 2025/09/19 14:07:33 by mcuenca-         ###   ########.fr       */
+/*   Updated: 2025/09/22 11:15:40 by mcuenca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,231 +17,80 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-t_bool	fake_exec(t_env *mini_env, t_list **pars)
+int	fake_exec(t_env *mini_env, t_list **pars)
 {
 	char	**mini_env_arr;
 
 	if (!expander(mini_env, pars))
-		return (FALSE);
+		return (0);
 	if (!quote_removal(pars))
-		return (FALSE);
+		return (0);
 	mini_env_arr = env_to_array(mini_env);
 	if (!mini_env_arr)
-		return (FALSE);
+		return (0);
 	ft_free_2p(mini_env_arr);
+	return (1);
+}
+
+void	clean_mng(char **cmmd, t_list **lex, t_list **pars)
+{
+	if (*lex)
+		ft_lstclear(lex, &del_t_token);
+	if (*pars)
+		ft_lstclear(pars, &del_t_cmmd);
+	if (*cmmd)
+		free(*cmmd);
+}
+
+void	init_minishell(t_env *mini_env, char **cmmd, t_list **lex, t_list **pars)
+{
+	while (1)
+	{
+		*cmmd = readline("minishell-");
+		if (!*cmmd)
+			return ;
+		add_history(*cmmd);
+		*lex = lexer(*cmmd);
+		if (!*lex)
+			return ;
+		*pars = parser(lex);
+		if (!*pars)
+			return ;
+		mini_env->r = fake_exec(mini_env, pars);
+		if (!mini_env->r)
+			return ;
+		if (ft_strncmp(*cmmd, "exit", 5) == 0)//
+			break ;//
+		clean_mng(cmmd, lex, pars);
+	}
+}
+
+t_bool	env_mng(t_env *mini_env, char **envp)
+{
+	mini_env->vars = env_dup(envp);
+	if (!mini_env->vars)
+		return (malloc_err(), FALSE);
+	mini_env->r = 0;
 	return (TRUE);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	char	*cmmd;
-	t_env	mini_env;
 	t_list	*lex;
 	t_list	*pars;
+	char	*cmmd;
+	t_env	mini_env;
 
+	(void)argv;
 	if (starter_err(argc, envp))
 		return (1);
-	(void)argv;
-	mini_env.vars = env_dup(envp);
-	if (!mini_env.vars)
-		return (malloc_err(), 1);
-	mini_env.r = 0;
 	lex = NULL;
 	pars = NULL;
-	while (1)
-	{
-		cmmd = readline("minishell-");
-		if (!cmmd)
-			break ;
-		lex = lexer(cmmd);
-		if (!lex)
-			break ;
-		pars = parser(&lex);
-		if (!pars)
-			break ;
-		ft_lstclear(&lex, &del_t_token);
-		if (!fake_exec(&mini_env, &pars))
-			break ;
-		ft_lstclear(&pars, &del_t_cmmd);
-		if (ft_strncmp(cmmd, "exit", 5) == 0)
-			break ;
-		free(cmmd);
-	}
-	printf("exit");
-	if (cmmd)
-		free(cmmd);
-	ft_lstclear(&lex, &del_t_token);
-	ft_lstclear(&pars, &del_t_cmmd);
+	if (!env_mng(&mini_env, envp))
+		return (1);
+	init_minishell(&mini_env, &cmmd, &lex, &pars);
+	clean_mng(&cmmd, &lex, &pars);//
 	ft_lstclear(&mini_env.vars, del_char_ptr);
 	rl_clear_history();
 	return (0);
 }
-
-/*** *** *** *** *** *** *** *** *EXPANDER *** *** *** *** *** *** *** *** ***/
-
-/*t_bool fake_exec(t_env *mini_env, t_list **pars)
-{
-	char	**mini_env_arr;
-
-	if  (!expander(mini_env, pars))
-		return (FALSE);
-	if (!quote_removal(pars))
-		return (FALSE);
-	mini_env_arr = env_to_array(mini_env);
-	if (!mini_env_arr)
-		return (FALSE);
-	ft_free_2p(mini_env_arr);
-	return (TRUE);
-}
-
-int	main(int argc, char **argv, char **envp)
-{
-	char	*cmmd;
-	t_env	mini_env;
-	t_list	*lex;
-	t_list	*pars;
-
-	if (argc != 1)
-		return (1);//starter_error
-	if (!envp)
-		return (1);//starter_error
-	(void)argv;
-	mini_env.vars = env_dup(envp);
-	if (!mini_env.vars)
-		return (1);//msg
-	mini_env.r = 0;
-	lex = NULL;
-	pars = NULL;
-	while (1)
-	{
-		cmmd = readline("minishell-");
-		if (!cmmd)
-			break ;
-		lex = lexer(cmmd);
-		if (!lex)
-			break ;
-		pars = parser(&lex);
-		if (!pars)
-			break ;
-		print_tokens(lex, TRUE, 0);
-		ft_lstclear(&lex, &del_t_token);
-		printf("\n***\n");
-		if (!fake_exec(&mini_env, &pars))
-			break ;
-		print_cmmds(pars, TRUE, 0);
-		ft_lstclear(&pars, &del_t_cmmd);
-		if (ft_strncmp(cmmd, "exit", 5) == 0)
-			break ;
-		free(cmmd);
-	}
-	printf("exit");
-	if (cmmd)
-		free(cmmd);
-	ft_lstclear(&lex, &del_t_token);
-	ft_lstclear(&pars, &del_t_cmmd);
-	ft_lstclear(&mini_env.vars, del_char_ptr);
-	rl_clear_history();
-	return (0);
-}*/
-
-/*** *** *** *** *** *** *** *** *PARSER * *** *** *** *** *** *** *** *** ***/
-
-/*int	main(int argc, char **argv, char **envp)
-{
-	char	*cmmd;
-	t_env	minishell_env;
-	t_list	*lex;
-	t_list	*pars;
-
-	if (argc != 1)
-		return (1);//starter_error
-	if (!envp)
-		return (1);//starter_error
-	(void)argv;
-	minishell_env.vars = env_dup(envp);
-	if (!minishell_env.vars)
-		return (1);//msg
-	minishell_env.r = 0;
-	lex = NULL;
-	pars = NULL;
-	while (1)
-	{
-		cmmd = readline("minishell-");
-		if (!cmmd)
-			break ;
-		lex = lexer(cmmd);
-		if (!lex)
-			break ;
-		pars = parser(&lex);
-		if (!pars)
-			break ;
-		ft_lstclear(&lex, &del_t_token);
-		ft_lstclear(&pars, &del_t_cmmd);
-		if (ft_strncmp(cmmd, "exit", 5) == 0)
-			break ;
-		free(cmmd);
-	}
-	printf("exit");
-	if (cmmd)
-		free(cmmd);
-	ft_lstclear(&lex, &del_t_token);
-	ft_lstclear(&pars, &del_t_cmmd);
-	ft_lstclear(&minishell_env.vars, del_char_ptr);
-	rl_clear_history();
-	return (0);
-}*/
-
-/*** *** *** *** *** *** *** *** * LEXER * *** *** *** *** *** *** *** *** ***/
-/*int	main(int argc, char **argv, char **env)
-{
-	char	*cmmd;
-	t_list	*env_struct;
-	t_list	*lex;
-
-	if (argc != 1)
-		return (0);
-	(void)argv;
-	env_struct = env_dup(env);
-	if (!env_struct)
-		return (1);
-	while (1)
-	{
-		cmmd = readline("minishell>");
-		if (!cmmd)
-			break ;
-		lex = lexer(cmmd);
-		print_tokens(lex, TRUE, 0);
-		ft_lstclear(&lex, del_t_token);
-		if (ft_strncmp(cmmd, "exit", 5) == 0)
-			break ;
-		free(cmmd);
-	}
-	printf("exit");
-	if (cmmd)
-		free(cmmd);
-	ft_lstclear(&env_struct, del_t_env);
-	rl_clear_history();
-	return (0);
-
-}*/
-
-/*** *** *** *** *** *** *** *** *** ENV *** *** *** *** *** *** *** *** ***/
-/*int	main(int argc, char **argv, char **env)
-{
-	t_list	*env_struct;
-	t_list	*tmp;
-
-	if (!env_struct)
-		return (1);
-	(void)argv;
-	env_struct = env_dup(env);
-	if (!env_struct)
-		return (1);
-	tmp = env_struct;
-	while (tmp)
-	{
-		printf("%s\n", (char *)tmp->content);
-		tmp = tmp->next;
-	}
-	ft_lstclear(&env_struct, del_t_env);
-}*/
