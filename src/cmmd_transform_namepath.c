0b386bc2b4e99e67c47233d4e7b6cf0d4a0e3b19
Francesc Aguirre Plans <faguirre@cbr4s2.42barcelona.com>
2025-10-20 19:49:56 +0200

    [fix]: merge conflict

--- src/cmmd_transform_namepath.c
+++ src/cmmd_transform_namepath.c
@@ -6,7 +6,7 @@
 /*   By: faguirre <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 03:35:58 by faguirre          #+#    #+#             */
-/*   Updated: 2025/10/16 17:02:40 by faguirre         ###   ########.fr       */
+/*   Updated: 2025/10/20 19:46:08 by faguirre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,10 @@ static int	split_envpath(t_env *env, char ***path_split)
 {
 	char	*path_str;
 
-	path_str = obtain_env_var_value(env, "$PATH");
+	path_str = obtain_env_var_value(env, NULL, "PATH");
 	if (!path_str)
 		get_error(env, ST_ERR, "Error: env $PATH not found");
 	*path_split = ft_split(path_str, ':');
-	free(path_str);
 	if (!path_split)
 		return (get_error(env, ST_ERR_MALLOC, NULL));
 	return (1);
